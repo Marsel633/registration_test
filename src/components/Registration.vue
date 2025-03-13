ec46035d9016fe4e629f3c112728e86ec5e10387
Marsel633 <akparov1m@gmail.com>
2025-03-13 21:39:50 +0600

    added config for ts and toastify

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,34 +1,67 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+import { toast } from 'vue3-toastify'
 
+const firstName = ref<string>('')
+const lastName = ref<string>('')
+const email = ref<string>('')
+const phone = ref<string>('')
+const password = ref<string>('')
+const repeatPassword = ref<string>('')
+
+const API = 'https://jsonplaceholder.typicode.com/posts'
+
+const handleSubmit = async () => {
+    try {
+        const response = await axios.post(API, {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            phone: phone.value,
+            password: password.value,
+            repeatPassword: repeatPassword.value,
+        })
+
+        toast.success('Вы успешно зарегистрировались!')
+    } catch (error: unknown) {
+        console.error(error)
+        if(error) {
+            // toast.error(`Ошибка: ${error.response?.data?.message || error.message}`)
+        } else {
+            toast.error('Произошла непредвиденная ошибка')
+        }
+    }
+}
 </script>
 
 <template>
     <section class="container">
         <h2 class="title">Регистрация</h2>
-        <form class="form" action="">
+        <form @submit.prevent="handleSubmit" class="form">
             <div class="form__item">
                 <label for="">Имя</label>
-                <input type="text">
+                <input type="text" v-model="firstName" />
             </div>
             <div class="form__item">
                 <label for="">Фамилия</label>
-                <input type="text">
+                <input type="text" v-model="lastName" />
             </div>
             <div class="form__item">
                 <label for="">Почта</label>
-                <input type="text">
+                <input type="emial" v-model="email" />
             </div>
             <div class="form__item">
                 <label for="">Номер</label>
-                <input type="text">
+                <input type="text" v-model="phone" />
             </div>
             <div class="form__item">
                 <label for="">Пароль</label>
-                <input type="text">
+                <input type="password" v-model="password" />
             </div>
             <div class="form__item">
                 <label for="">Подтвердите пароль</label>
-                <input type="text">
+                <input type="password" v-model="repeatPassword" />
             </div>
             <button class="submitBtn" type="submit">Зарегистрироваться</button>
         </form>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import Vue3Toastify from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
+import './style.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(Vue3Toastify, {
+    autoClose: 3000,
+    position: 'top-right',
+    theme: 'dark',
+  })
+  .mount('#app')
